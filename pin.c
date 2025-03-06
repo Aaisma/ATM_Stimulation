878c001e5ac78d2505072323f1e18c9f3bbbe580
Aaisma <wagleaaisma07@gmail.com>
2025-03-06 04:47:34 +0000

    Final commit

--- pin.c
+++ pin.c
@@ -1,17 +1,16 @@
-#Code for ATM Stimulation
-
 #include <stdio.h>
+#include <conio.h>
 
-int pin = 1234;
-float balance = 1000.0;
-
+int pin = 1234; //The pin that needs to be entered
+float balance = 1000.0; //The initial balance
+ 
 void checkBalance() {
-    printf("Your balance is: $%.2f\n", balance);
+    printf("\nYour balance is: $%.2f\n", balance);
 }
 
 void depositMoney() {
     float amount;
-    printf("Enter amount to deposit: ");
+    printf("\nEnter amount to deposit: ");
     scanf("%f", &amount);
     balance += amount;
     printf("\nDeposit successful. New balance: $%.2f\n", balance);
@@ -19,33 +18,58 @@ void depositMoney() {
 
 void withdrawMoney() {
     float amount;
-    printf("Enter amount to withdraw: ");
+    printf("\nEnter amount to withdraw: ");
     scanf("%f", &amount);
     if (amount <= balance) {
         balance -= amount;
-        printf("\nWithdrawal successful. New balance: $%.2f\n", balance);
+        printf("\nWithdrawal successful.\n\nNew balance: $%.2f\n", balance);
     } else {
         printf("\nInsufficient balance.\n");
     }
 }
 
+// Function to read PIN and mask it with '****'
+int readPIN() {
+    int pinEntered = 0;
+    printf("\n");
+    printf("Enter your PIN: ");
+    
+    for (int i = 0; i < 4; i++) {
+        char ch = getch();
+        printf("*");
+        
+        int digit = ch - '0';
+
+        //Builds the pin from 0 as first digit
+        pinEntered = pinEntered * 10 + digit;
+    }
+    
+    printf("\n");
+    
+    return pinEntered;
+}
+
 int main() {
     int user_pin, choice;
     
-    // PIN authentication
-    printf("Enter your PIN: ");
-    scanf("%d", &user_pin);
+    // PIN Authentication
+    user_pin = readPIN();
     if (user_pin != pin) {
         printf("Invalid PIN.\n");
+        printf("\n");
         return 1;
     }
 
     do {
-        printf("\n1. Check Balance\n");
+        //User Menu
+        printf("\n");
+        printf("1. Check Balance\n");
         printf("2. Deposit Money\n");
         printf("3. Withdraw Money\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
+        printf("4. Exit");
+        printf("\n");
+
+        printf("\nEnter your choice: ");
         scanf("%d", &choice);
         
         switch (choice) {
@@ -59,12 +83,14 @@ int main() {
                 withdrawMoney();
                 break;
             case 4:
-                printf("Thank You for the Transaction....\n");
+                printf("Exiting...\n\n");
                 break;
             default:
-                printf("Invalid choice. Please Choose between 1 to 4...\n");
+                printf("\nInvalid choice.Try again\n");
         }
-    } while (choice != 4);
+    } while (choice != 4); //Executes when user chooses something other than what's on the list
 
     return 0;
 }
+
+
